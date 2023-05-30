a5ce4ebd12cabacf1a8943b68d4a47b5416cdc88
Hong Le <hongle622@gmail.com>
2023-05-30 11:24:40 -0700

    screen is displayed

---
+++ settings.py
@@ -0,0 +1,15 @@
+import pygame as pg
+
+vec = pg.math.Vector2
+
+FPS = 60
+FIELD_COLOR = (31, 31, 31)
+BG_COLOR = (24, 89, 117)
+
+# one tile peice of the game field 
+TILE_SIZE = 50
+
+# 10 by 20 for the game field
+FEILD_SIZE = FIELD_W, FIELD_H = 10, 20
+FIELD_RES = FIELD_W * TILE_SIZE, FIELD_H * TILE_SIZE
+
