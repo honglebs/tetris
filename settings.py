fabc667a37f2d738b5e0fa74e9b5adb363d190ac
Hong Le <hongle622@gmail.com>
2023-06-15 12:10:25 -0700

    added inital start for tetrominoes

--- settings.py
+++ settings.py
@@ -10,15 +10,17 @@ FIELD_COLOR = (31, 31, 31)
 TILE_SIZE = 50
 
 # 10 by 20 for the game field
-FEILD_SIZE = FIELD_W, FIELD_H = 10, 20
+FIELD_SIZE = FIELD_W, FIELD_H = 10, 20
 FIELD_RES = FIELD_W * TILE_SIZE, FIELD_H * TILE_SIZE
+
+INIT_POS_OFFSET = vec(FIELD_SIZE) // 2
 
 TETROMINOES = {
     'T': [(0, 0), (-1, 0), (1, 0), (0, -1)],
     'O': [(0, 0), (0, -1), (1, 0), (1, -1)], 
-    'J': [],
-    'L': [],
-    'I': [],
-    'S': [],
-    'Z': []
+    'J': [(0, 0), (-1, 0), (0, -1), (0, -2)],
+    'L': [(0, 0), (1, 0), (0, -1), (0, -2)],
+    'I': [(0, 0), (0, 1), (0, -1), (1, -1)],
+    'S': [(0, 0), (-1, 0), (0, -1), (1, -1)],
+    'Z': [(0, 0), (1, 0), (0, -1), (-1, -1)]
 }
