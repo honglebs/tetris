fc6c56a1766c29e2d2f3ec733a056ec6386040c4
Hong Le <hongle622@gmail.com>
2023-06-17 10:26:06 -0700

    restarting cause I dont get it

--- main.py
+++ main.py
@@ -1,36 +1,8 @@
-from settings import *
-from tetris import Tetris
-import sys
+import pygame as pg
 
-class App: 
-    def __init__(self):
-        pg.init()
-        pg.display.set_caption("Tetris")
-        self.screen = pg.display.set_mode(FIELD_RES)
-        self.clock = pg.time.Clock()
-        self.tetris = Tetris(self)
-    
-    def update(self):
-        self.tetris.update()
-        self.clock.tick(FPS)
-    
-    def draw(self):
-        self.screen.fill(color=FIELD_COLOR)
-        self.tetris.draw()
-        pg.display.flip()
-    
-    def check_events(self):
-        for event in pg.event.get():
-            if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
-                pg.quit()
-                sys.exit()
+pg.init()
 
-    def run(self):
-        while True:
-            self.check_events()
-            self.update()
-            self.draw()
+screen = pg.display.set_mod((300, 600))
+pg.display.set_caption("Python Tetris")
 
-if __name__ == '__main__':
-    app = App()
-    app.run()
+clock = pg.time.Clock()
