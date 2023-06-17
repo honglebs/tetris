50cb022b9b70046ab9469a3324d12d7a5c75513e
Hong Le <hongle622@gmail.com>
2023-06-17 10:54:23 -0700

    made background dark blue

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
-import pygame
+import pygame as pg
 import sys
 
-pygame.init()
+pg.init()
+dark_blue = (44, 44, 127)
 
-screen = pygame.display.set_mode((300, 600))
-pygame.display.set_caption("Python Tetris")
+screen = pg.display.set_mode((300, 600))
+pg.display.set_caption("Python Tetris")
 
-clock = pygame.time.Clock()
+clock = pg.time.Clock()
 
 # start of the game loop (check 3 things)
 while True:
-      for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
+    # event listener 
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            pg.quit()
+            sys.exit()
 
-# 
+    # Drawing
+    screen.fill(dark_blue)            
+    pg.display.update()
+    clock.tick(60)
+      
