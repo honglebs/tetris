f204ae541654ff27761c61a1845285e006101e10
Hong Le <hongle622@gmail.com>
2023-06-19 12:32:57 -0700

    finished the user interface

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ pg.init()
 
 title_font = pg.font.Font(None, 40)
 score_surface = title_font.render("Score", True, Colors.white)
+next_surface = title_font.render("Next", True, Colors.white)
+game_over_surface = title_font.render("GAME OVER", True, Colors.white)
 
 score_rect = pg.Rect(320, 55, 170, 60)
+next_rect = pg.Rect(320, 215, 170, 180)
 
 screen = pg.display.set_mode((500, 620))
 pg.display.set_caption("Tetris")
@@ -45,7 +48,14 @@ while True:
     # Drawing
     screen.fill(Colors.dark_blue)
     screen.blit(score_surface, (365, 20, 50, 50))
+    screen.blit(next_surface, (375, 180, 50, 50))
+
+    if game.game_over == True:
+        screen.blit(game_over_surface, (320, 450, 50, 50))
+
+
     pg.draw.rect(screen, Colors.light_blue, score_rect, 0, 10)
+    pg.draw.rect(screen, Colors.light_blue, next_rect, 0, 10)
     game.draw(screen)
 
     # Updating
