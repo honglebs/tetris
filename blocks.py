a28bb36f16c3cd7334aea3917fcdcfdd33fe68dd
Hong Le <hongle622@gmail.com>
2023-06-18 22:32:37 -0700

    added TBlock Positions

--- blocks.py
+++ blocks.py
@@ -30,3 +30,13 @@ class IBlock(Block):
             2: [Position(2, 0), Position(2, 1), Position(2, 2), Position(2, 3)],
             3: [Position(0, 1), Position(1, 1), Position(2, 1), Position(3, 1)]
         }
+
+class TBlock(Block):
+    def __init__(self):
+        super().__init__(id = 4)
+        self.cells = {
+            0: [Position(0, 1), Position(1, 0), Position(1, 1), Position(1, 2)],
+            1: [Position(0, 1), Position(1, 1), Position(1, 2), Position(2, 1)],
+            2: [Position(1, 0), Position(1, 1), Position(1, 2), Position(2, 1)],
+            3: [Position(0, 1), Position(1, 0), Position(1, 1), Position(2, 1)]
+        }
