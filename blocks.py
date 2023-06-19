35a8119c0be8e113999450bce1c844ff9430f486
Hong Le <hongle622@gmail.com>
2023-06-18 22:53:32 -0700

    finished 7 tetrominoes positions

--- blocks.py
+++ blocks.py
@@ -40,3 +40,33 @@ class TBlock(Block):
             2: [Position(1, 0), Position(1, 1), Position(1, 2), Position(2, 1)],
             3: [Position(0, 1), Position(1, 0), Position(1, 1), Position(2, 1)]
         }
+
+class OBlock(Block):
+    def __init__(self):
+        super().__init__(id = 5)
+        self.cells = {
+            0: [Position(0, 0), Position(0, 1), Position(1, 0), Position(1, 1)],
+            1: [Position(0, 0), Position(0, 1), Position(1, 0), Position(1, 1)], 
+            2: [Position(0, 0), Position(0, 1), Position(1, 0), Position(1, 1)],
+            3: [Position(0, 0), Position(0, 1), Position(1, 0), Position(1, 1)]
+        }
+
+class SBlock(Block): 
+    def __init__(self):
+        super().__init__(id = 6)
+        self.cells = {
+            0: [Position(0, 1), Position(0, 2), Position(1, 0), Position(1, 1)],
+            1: [Position(0, 1), Position(1, 1), Position(1, 2), Position(2, 2)],
+            2: [Position(1, 1), Position(1, 2), Position(2, 0), Position(2, 1)],
+            3: [Position(0, 0), Position(1, 0), Position(1, 1), Position(2, 1)],
+        }
+
+class ZBlock(Block): 
+    def __init__(self):
+        super().__init__(id = 7)
+        self.cells = {
+            0: [Position(0, 0), Position(0, 1), Position(1, 1), Position(1, 2)],
+            1: [Position(0, 2), Position(1, 1), Position(1, 2), Position(2, 1)],
+            2: [Position(1, 0), Position(1, 1), Position(2, 1), Position(2, 2)],
+            3: [Position(0, 1), Position(1, 0), Position(1, 1), Position(2, 0)],
+        }
